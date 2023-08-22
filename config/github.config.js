a1a99aee6451c859d0e96063f966ed55cd2411ad
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-08-22 16:56:23 +0530

    update url

--- config/github.config.js
+++ config/github.config.js
@@ -10,7 +10,7 @@ export default (passport) => {
         new GithubStrategy({
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: `/auth/github/callback`
+            callbackURL: `https://xerocodeeassignment.onrender.com/auth/github/callback`
         },
             async (accessToken, refreshToken, profile, done) => {
                 // console.log("accessToken");
