9fb07ef64ab0eb4825278a91ac090c1d422a1fc9
TARA CHAND KUMAWAT <tarachand122333@gmail.com>
2023-08-27 08:09:05 +0530

    all done

--- config/githubrepo.config.js
+++ config/githubrepo.config.js
@@ -10,7 +10,7 @@ export default (passport) => {
         new GithubStrategy({
             clientID: process.env.GITHUB_CLIENT_ID_REPO,
             clientSecret: process.env.GITHUB_CLIENT_SECRET_REPO,
-            callbackURL: `https://xerocodeeassignment.onrender.com/auth/github/repo`
+            callbackURL: `https://xerocodeeassignment.onrender.com/auth/github`
         },
             async (accessToken, refreshToken, profile, done) => {
                 const newUser = {
